8bf244c2d83a24663e7c8ce3248af5e2fb402ae7
Matt <mattray0295@gmail.com>
2017-09-25 17:29:34 -0500

    finished change and click

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,11 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  
 
-})
+  $('span').on('click', function() {
+    var time = new Date(Date.now());      // so it can reset variable
+    var text = `You clicked: ${$(this).text()} at ${time}`;
+    $('ul').append(`<li>${text}</li>`);
+  });
+});
