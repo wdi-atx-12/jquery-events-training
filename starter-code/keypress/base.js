be6242ec644d35f956e5ff79b1f7a97e78cd1091
Matt <mattray0295@gmail.com>
2017-09-25 17:39:20 -0500

    finished keypress

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,23 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+var startTime;
+var endTime;
+var total = 0;
 
-})
+$(document).ready(function(){
+  $(window).on("keypress", () => {
+    if ( startTime ) {
+      endTime = Date.now();
+      total = (endTime - startTime) / 1000;
+      displayTotal();
+    } else {
+      startTime = Date.now();
+      window.setInterval(displayTotal, 75);
+    }
+  });
+});
+
+function displayTotal() {
+  total = (Date.now() - startTime) / 1000;
+  $("#total-time").text(`${total} seconds`);
+}
