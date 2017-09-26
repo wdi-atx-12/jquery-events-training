34c4c72a6ab9ecccefe782bd5bbecdad05f4b13a
Peter Kong <bansuki@gmail.com>
2017-09-25 21:37:39 -0500

    timer works and displays second increments

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,31 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  let isRunning = false;
+  let timerInterval;
+  $(document).keypress(function (e) {
+    console.log('asdf');
+    if(e.keyCode == 32) {
+      isRunning = !isRunning;
+      if (isRunning) {
+        let startMS = Date.now();
+        timerInterval = window.setInterval(function(){
+          $('#total-time').text(getMinSecs(startMS, Date.now()));
+        }, 1000);
+      } else {
+        window.clearInterval(timerInterval);
+      }
+    }
+  });
+});
 
-})
+function getMinSecs(startMS, endMS) {
+  let elapsedSeconds = (endMS - startMS)/1000;
+  let secs = Math.floor(elapsedSeconds%60);
+  let mins = Math.floor(elapsedSeconds/60);
+  return `${padDigits(mins)}:${padDigits(secs)}`;
+}
+
+function padDigits(num) {
+  return `${num < 10 ? '0' + num : num}`;
+}
