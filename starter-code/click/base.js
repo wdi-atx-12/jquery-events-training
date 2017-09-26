6b3f4a9401064cd6596ee4402233dbeb37517a34
Carrington Simecheck <c.simecheck89@yahoo.com>
2017-09-26 07:42:25 -0500

    the rest

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  $('span')on('click', function controlClick(clicky){
+    var spanText = 'You clicked ' + $(this).text() + ' at ' Date.now
+    $('ul').append('<li>' + spanText + "</li>")
+  })
 })
