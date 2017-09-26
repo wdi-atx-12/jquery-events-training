6e99ccef102878a179c19d78d19759f4d86ba866
Karla Moreno <moreno_karla@live.com>
2017-09-26 07:11:57 -0500

    added code that adds "did a thing" to list

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,10 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  // was able to add item to list but not selected span item, not sure how to do this without changing html :(
+  $('#imperatives').click(function(){
+    var addToList = $('span');
+    $('ul').append('<li>did a thing</li>')
+  });
+
 })
