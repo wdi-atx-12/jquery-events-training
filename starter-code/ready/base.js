ce5155162a6c47a82803cdf3b5786b874ecfae9c
thrakc3001 <cainmusicteach@gmail.com>
2017-09-25 20:27:23 -0500

    ready

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
   console.log( "The page says:", $("h1").text() );
 });
 
-$("h1").text("Go!");
+$(function() {
+    $( "h1" ).text( "Go!" );
+  });
 
 if ( $("h1").text() === "Go!" ){
   console.log( "The page says:", $("h1").text() );
