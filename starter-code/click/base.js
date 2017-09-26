f875748a636489c84cf93adc6125817c8a47377a
Peter Kong <bansuki@gmail.com>
2017-09-25 21:03:45 -0500

    got clicks working

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,9 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  $('div span').click(function() {
+    let li = document.createElement('li');
+    li.innerText = `"${$( this ).text()}" at ${Date.now()}`;
+    $('ul').append(li);
+  });
 })
