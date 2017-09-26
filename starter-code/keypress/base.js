ac4a75004fdbaafe7bf07fd3dc1946fef773cf1e
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-09-25 22:45:15 -0500

    adding keystroke stopwatch exercise

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -2,4 +2,32 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+var starts = null;
+var ends = null;
+
+document.body.onkeyup = function(spacey){
+
+    if(spacey.keyCode == 32){
+
+          if (starts === null){
+              starts = Date.now();
+              console.log("i was null " + starts);
+              return starts;
+          } else if (starts !== null && ends === null)  {
+              ends = Date.now();
+              var diff = (ends-starts)*.001;
+              $("#total-time").text(diff + " seconds");
+          } else if (starts !== null && ends !== null){
+               location.reload();
+          } else {
+              console.log('some error happened.');
+          }
+
+    }
+}
+
+
+
+
+
 })
