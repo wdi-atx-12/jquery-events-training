0144f2da66271180cef46b828e103a4a5e4dce32
Natasha Robarge <robarge.natasha7@gmail.com>
2017-09-26 07:43:17 -0500

    Submitting homework

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,9 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+$(document).ready(() => {
 
-})
+  $(`span`).on(`click`, (evt) => {
+    let textList = `Clicked: ${$(this).text()} at ${Date.now()}`;
+      $(`ul`).append(`<li> ${textList} </li>`);
+  });
+});
--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,20 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+var start;
+var end;
+var sum = 0;
 
-})
+$(document).ready(() => {
+  $(window).on(`keypress`, (evt) => {
+    if(start) {
+      end = Date.now(); //sets end time to current Date
+      sum = end - start / 1000;
+      displaySum();
+    }
+  });
+});
+
+let displaySum = () => {
+  sum = Date.now() - start / 1000;
+  $(`#total-time`).text(`${sum} seconds.`);
+}
