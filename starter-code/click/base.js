e718fdafd79062ca581412749da1ff30fdd244dc
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-09-25 23:46:45 -0500

    submitting HW for Wesley Maffly-Kipp

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,9 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  $("#imperatives > span").on("click", event => {
+    let text = event.currentTarget.innerText;
+    let readyText = $("<li></li>").text(text);
+    $("ul").append(readyText);
+  });
 })
