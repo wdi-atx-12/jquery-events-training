955fba20eebf7d7a7e6ef40bbc76d59edfa9d830
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-09-25 21:20:42 -0500

    adding change event

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -2,6 +2,16 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  $("#time").text( Date.now() );
+    $( "#right, #left" ).change(function() { //when the right field is changed fire the event
+
+          var field1 = $("#left").val();
+          var field2 = $("#right").val();
+          var field3 = parseInt(field1) + parseInt(field2);
+          $("#total").val(field3);
+
+});
+
+
+    $("#time").text( Date.now() );
 
 })
