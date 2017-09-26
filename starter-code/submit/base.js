7e1125de5a9e09d89732f829d0535d0252a5904b
Peter Kong <bansuki@gmail.com>
2017-09-25 20:27:16 -0500

    moved submit fnction inside ready

--- starter-code/submit/base.js
+++ starter-code/submit/base.js
@@ -6,9 +6,8 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
-});
-
-$('form').submit(function(e) {
-  e.preventDefault();
-  $('body').append(rick_roll_youtube_embed_code);
+  $("form").submit(function(e) {
+    e.preventDefault();
+    $("body").append(rick_roll_youtube_embed_code);
+  });
 });
