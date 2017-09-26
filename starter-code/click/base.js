55f2cb9f7374a3ac3aa30552d4701198b78d08e6
thrakc3001 <cainmusicteach@gmail.com>
2017-09-26 08:12:20 -0500

    click

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,9 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+	$("span").on("click", function clicker(event){
+    	var clickText = "You Clicked: " + $(this).text() + " at " + Date.now();
+    	$("ul").append("<li>" + clickText + "</li>")
 
+    })
 })
