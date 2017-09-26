bfa9db0e5f9de83b59c1302208bf4f576d6e2caa
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-09-25 22:42:34 -0500

    change text to html

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   console.log( "The page says:", $("h1").text() );
 });
 
-$("h1").text("Go!");
+$("h1").html("Go!");
 
 if ( $("h1").text() === "Go!" ){
   console.log( "The page says:", $("h1").text() );
