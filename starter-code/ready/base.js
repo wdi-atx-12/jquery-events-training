35cd35a6ae9485d90dc45c7a3df00549404cb517
CescoIV <crordaz@gmail.com>
2017-09-25 22:14:35 -0500

    page says go

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -3,13 +3,13 @@
 */
 
 $(document).ready(function(){
-  console.log( "The page says:", $("h1").text() );
+	$('h1').text(function(i,oldText){
+		return oldText === "Ready?" ? 'Go!' : oldText;
+	});
 });
 
-$("h1").text("Go!");
-
-if ( $("h1").text() === "Go!" ){
-  console.log( "The page says:", $("h1").text() );
+if($("h1").text() === "Go!"){
+  console.log( "The page says:", $("h1").text());
   console.log("Success!");
 } else {
   console.log( 'Can you make it say "Go!"?' );
