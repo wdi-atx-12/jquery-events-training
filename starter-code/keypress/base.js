62bf85eb8e663085d299f05dbc476d8b1675852e
thrakc3001 <cainmusicteach@gmail.com>
2017-09-26 08:20:29 -0500

    keypress

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,21 @@
-console.log("Sanity Check: JS is working!");
+var starting;
+var ending; 
+var total = 0;
 
 $(document).ready(function(){
-
+  $(window).on("keypress", function keypresser(event){
+    if ( starting ) {
+      ending = Date.now();
+      total = (ending - starting) / 1000;
+      displayTotal();
+    } else {
+      starting = Date.now();
+      window.setInterval(totality, 500);
+    }
+  })
 })
+
+	function totality(){
+	  total = (Date.now() - starting) / 1000;
+	  $("#total-time").text( total + " seconds" );
+	}
