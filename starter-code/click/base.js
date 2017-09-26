49c9166c399fe287ed956cf7e9609e998f8a1834
Karla Moreno <moreno_karla@live.com>
2017-09-26 07:15:57 -0500

    added timestamp on click

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
   // was able to add item to list but not selected span item, not sure how to do this without changing html :(
   $('#imperatives').click(function(){
     var addToList = $('span');
-    $('ul').append('<li>did a thing</li>')
+    var now = new Date();
+    $('ul').append('<li>did a thing</li>' + 'on ' + now);
   });
 
 })
