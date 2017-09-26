423f75b22e48cee291bba284bb3de927e20f81d6
Karla Moreno <moreno_karla@live.com>
2017-09-26 06:54:07 -0500

    move 1 line of code into ready function to change text from "ready?" to "go!" when document is "ready"

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -3,9 +3,11 @@
 */
 
 $(document).ready(function(){
+  $("h1").text("Go!");
   console.log( "The page says:", $("h1").text() );
 });
 
+// moved this line of code into .ready func so that when document is 'ready', h1 text changes to 'go!''
 $("h1").text("Go!");
 
 if ( $("h1").text() === "Go!" ){
